ffe1a03a17eaf72e6620310fc3fe4db5c186f3a3
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-03-16 23:05:38 +1100

    basic query working

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var fetch = require('isomorphic-fetch')
+var express = require('express')
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'hooray! welcome to our api!' });
-});
+const access_token = 'EAACEdEose0cBAFk3fosTiz7X8AvZC7lzWVqSuewZBFd6tJUPNripJReOhL0zpbdBIpQfNZBQgZAauD1MhmEDUrNrqSiBBYyE317nVgdudTDo9562a9SVZBzqtH8pujhXsF7qwgvBHdcmHlvpIKMZCv4Y3MRO9enG24umSX3XevnulSlVIOPihbhD9sm7D2qffiqQZC5mdcUWwZDZD'
 
-module.exports = router;
+router.get('/', function (req, res, next) {
+  fetch(`https://graph.facebook.com/woolworths?access_token=EAACEdEose0cBAFk3fosTiz7X8AvZC7lzWVqSuewZBFd6tJUPNripJReOhL0zpbdBIpQfNZBQgZAauD1MhmEDUrNrqSiBBYyE317nVgdudTDo9562a9SVZBzqtH8pujhXsF7qwgvBHdcmHlvpIKMZCv4Y3MRO9enG24umSX3XevnulSlVIOPihbhD9sm7D2qffiqQZC5mdcUWwZDZD`)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(json => {
+        res.json(json)
+      })
+    }
+  })
+  .catch(error => console.error(error))
+})
+
+module.exports = router
