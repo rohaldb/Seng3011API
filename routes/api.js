47fa9c7d314dd07184936452b8f0137c6650d1dc
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-03-26 20:28:24 +1100

    start and end date filter working

--- routes/api.js
+++ routes/api.js
@@ -6,15 +6,17 @@ var router = express.Router()
 const access_token = 'EAACEdEose0cBAOcAKM2oSP9X0KPZBdAiYBIWZCPqCTh7nZA1knFZCYgXsT8z7WuzZBMeO4xdglSYUA7kfW44ZBZCJdvaykGtF4TYRSypo5PLv7mXK272vjR6xEFhCjhGYfy9u94hgBLyrR08Dqex6NWfHpeajHrMRUGqq6OjSQkIuwwHHtiMHfzMssZA5c7J0y0ZD'
 
 router.get('/', function (req, res, next) {
-  // console.log(req)
   console.log(req.query)
+
+  const { company, statistics } = req.query
   const start_date = moment(req.query.start_date)
   const end_date = moment(req.query.end_date)
 
-  if (!start_date.isValid() || !end_date.isValid || !req.query.company) {
+  if (!start_date.isValid() || !end_date.isValid() || !company) {
+    // the params are invalid
     res.json({status: 400, message: 'bad request'})
   } else {
-    fetch(`https://graph.facebook.com/woolworths?access_token=${access_token}`)
+    fetch(`https://graph.facebook.com/${company}/posts?since=${start_date.unix()}&until=${end_date.unix()}&access_token=${access_token}`)
     .then(function (response) {
       if (response.ok) {
         response.json().then(json => {
