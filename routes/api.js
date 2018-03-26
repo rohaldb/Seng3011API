65c8f16f923846e01107b24790a87fca91c0566c
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-03-26 20:07:51 +1100

    reading params and checking if valid

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,29 @@
 var fetch = require('isomorphic-fetch')
 var express = require('express')
+var moment = require('moment')
 var router = express.Router()
 
-const access_token = 'EAACEdEose0cBAFk3fosTiz7X8AvZC7lzWVqSuewZBFd6tJUPNripJReOhL0zpbdBIpQfNZBQgZAauD1MhmEDUrNrqSiBBYyE317nVgdudTDo9562a9SVZBzqtH8pujhXsF7qwgvBHdcmHlvpIKMZCv4Y3MRO9enG24umSX3XevnulSlVIOPihbhD9sm7D2qffiqQZC5mdcUWwZDZD'
+const access_token = 'EAACEdEose0cBAOcAKM2oSP9X0KPZBdAiYBIWZCPqCTh7nZA1knFZCYgXsT8z7WuzZBMeO4xdglSYUA7kfW44ZBZCJdvaykGtF4TYRSypo5PLv7mXK272vjR6xEFhCjhGYfy9u94hgBLyrR08Dqex6NWfHpeajHrMRUGqq6OjSQkIuwwHHtiMHfzMssZA5c7J0y0ZD'
 
 router.get('/', function (req, res, next) {
-  fetch(`https://graph.facebook.com/woolworths?access_token=EAACEdEose0cBAFk3fosTiz7X8AvZC7lzWVqSuewZBFd6tJUPNripJReOhL0zpbdBIpQfNZBQgZAauD1MhmEDUrNrqSiBBYyE317nVgdudTDo9562a9SVZBzqtH8pujhXsF7qwgvBHdcmHlvpIKMZCv4Y3MRO9enG24umSX3XevnulSlVIOPihbhD9sm7D2qffiqQZC5mdcUWwZDZD`)
-  .then(function (response) {
-    if (response.ok) {
-      response.json().then(json => {
-        res.json(json)
-      })
-    }
-  })
-  .catch(error => console.error(error))
+  // console.log(req)
+  console.log(req.query)
+  const start_date = moment(req.query.start_date)
+  const end_date = moment(req.query.end_date)
+
+  if (!start_date.isValid() || !end_date.isValid || !req.query.company) {
+    res.json({status: 400, message: 'bad request'})
+  } else {
+    fetch(`https://graph.facebook.com/woolworths?access_token=${access_token}`)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(json => {
+          res.json(json)
+        })
+      }
+    })
+    .catch(error => console.error(error))
+  }
 })
 
 module.exports = router
