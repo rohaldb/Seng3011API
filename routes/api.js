9cf83e84a6b2dba8abc9c0b20e249716fa4b4c6f
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-03-29 18:34:54 +1100

    added dependency and clean up

--- routes/api.js
+++ routes/api.js
@@ -49,68 +49,66 @@ router.get('/post/:id', function (req, res, next) {
 /*
  * Facebook page information route for a company.
  */
- router.get('/:company', function (req, res, next) {
+router.get('/:company', function (req, res, next) {
      // start timer
-     const start_time = new Date()
-     let company = req.params.company
-     const statistics = req.query.statistics
-     const start_date = moment(req.query.start_date)
-     const end_date = moment(req.query.end_date)
+  const start_time = new Date()
+  const statistics = req.query.statistics
+  const start_date = moment(req.query.start_date)
+  const end_date = moment(req.query.end_date)
 
-     db.each(`SELECT *, COUNT(1) > 0
+  db.each(`SELECT *, COUNT(1) > 0
          FROM ListedCompanies
-         WHERE Code = '${company}' LIMIT 1`, (err, row) => {
-             if (err) {
-                 console.error(err.message);
-             }
-                // console.log(row.Code + "\t" + row.Company + "\t" + row.Industry);
-             if (row.Code != null) {
-                company = row.Company
-                // console.log(company)
-             }
-             if (!start_date.isValid() || !end_date.isValid()) {
-                 // check for valid date parameters
-                 res.json({status: 400, status_text: 'Invalid date parameters'})
-             } else if (!statistics) {
-                 // check for missing stats parameter
-                 res.json({status: 400, status_text: 'Missing parameter: `statistics`'})
+         WHERE Code = '${req.params.company}' LIMIT 1`, (err, row) => {
+    if (err) {
+      console.error(err.message)
+    }
+
+    const company = row.Code ? row.Company : req.params.company
+
+    if (!start_date.isValid() || !end_date.isValid()) {
+      // check for valid date parameters
+      res.json({status: 400, status_text: 'Invalid date parameters'})
+    } else if (!statistics) {
+      // check for missing stats parameter
+      res.json({status: 400, status_text: 'Missing parameter: `statistics`'})
+    } else {
+      fetch(graphAPIString(company, start_date, end_date, statistics))
+       .then(response => {
+         if (response.ok) {
+           response.json().then(data => {
+             res.json(responseFormatter(req, response, start_time, data))
+           })
+         } else {
+           return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
+         }
+       }).then(response => {
+         if (response) {
+           response.json().then(data => {
+             if (data.data[0]) {
+               const companyId = data.data[0].id
+               return fetch(graphAPIString(companyId, start_date, end_date, statistics))
              } else {
-                 fetch(graphAPIString(company, start_date, end_date, statistics))
-                 .then(response => {
-                     if (response.ok) {
-                         response.json().then(data => {
-                             res.json(responseFormatter(req, response, start_time, data))
-                         })
-                     } else {
-                         return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
-                     }
-                 }).then(response => {
-                     if (response) {
-                         response.json().then(data => {
-                             if (data.data[0]) {
-                                 const companyId = data.data[0].id
-                                 return fetch(graphAPIString(companyId, start_date, end_date, statistics))
-                             } else {
-                                 res.json(responseFormatter(req, response))
-                             }
-                         }).then(response => {
-                             if (response) {
-                                 response.json().then(data => {
-                                     if (data.error) {
-                                         res.json({status: 400, status_text: data.error.message})
-                                     } else {
-                                         res.json(responseFormatter(req, response, start_time, data))
-                                     }
-                                 })
-                             }
-                         })
-                     }
-                 }).catch(error => console.error(error))
+               res.json(responseFormatter(req, response))
              }
-
-         })
-
-     })
+           }).then(response => {
+             if (response) {
+               response.json().then(data => {
+                 if (data.error) {
+                   res.json({status: 400, status_text: data.error.message})
+                 } else {
+                   console.log('hi ben111')
+                   res.json(responseFormatter(req, response, start_time, data))
+                   console.log('hi ben')
+                 }
+               })
+             }
+           })
+         }
+       }).catch(error => console.error(error))
+    }
+  })
+  console.log('hi ben')
+})
 
 /*
  * Returns URL to pass to Facebook Graph API for company info.
@@ -126,26 +124,21 @@ const responseFormatter = (req, api_response, start_time, api_data) => {
   const end_time = new Date()
   return {
     data: api_data,
-    // metadata: {
-      dev_team: 'Team Unassigned',
-      version: '1.0.0',
-      start_time,
-      end_time,
-      time_elapsed: end_time - start_time,
-      params: req.query,
-      status: api_response.status,
-      status_text: api_response.statusText
-    // }
+    dev_team: 'Team Unassigned',
+    version: '1.0.0',
+    start_time,
+    end_time,
+    time_elapsed: end_time - start_time,
+    params: req.query,
+    status: api_response.status,
+    status_text: api_response.statusText
   }
 }
 
-let db = new sqlite3.Database('allcompanies.db', (err) => {
+const db = new sqlite3.Database('allcompanies.db', (err) => {
   if (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
-
-
-});
-
+})
 
 module.exports = router
