e35dcafcd455a828091fa94e5ad9cc2a41e7cdf3
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-03-30 00:58:43 +1100

    allow user to query for like and comment count, tidy output of api

--- routes/api.js
+++ routes/api.js
@@ -4,24 +4,25 @@ var moment = require('moment');
 var sqlite3 = require('sqlite3').verbose();
 var router = express.Router();
 
-// Use Heroku REDIS_URL or default to local redis server
-var REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379"; 
+/*
+ * Use Heroku REDIS_URL or default to local redis server.
+ */
+var REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 var cache = require('express-redis-cache')({
   client: require('redis').createClient(REDIS_URL)
 });
 
-const access_token = 'EAACEdEose0cBACz2JgeNU3V8CBuPZBSCbHo3Gnw6Jnm8GGxFNW6QbphiwZCZAlST18826JqzBhUKQCT4Dstro5BryFYjYpP1FL17ZCLlZCj3uFsxWqc37CwqhLTDJwtkDWAmoGTBdXGBQvAUKe1Yq7vMHs4VpyLUzHs9Gvez3GCKXa2n9tntQFWZCBM1hQh1XFuZCUS6XJt5QZDZD';
-const fb_version = 'v2.6';
+const access_token = 'EAACEdEose0cBALL3EkMUYMy5DtXTluEZCH85xODEht5xZB9YFWXDEZCRTEXStZBPDutqx2sJJz8pXw9u50JFAKSflewMp2dZAsPDG64kGeRacwj7n5CC09o4350FpeCyXgdJXxHTwPEYY1TZCxlM55ZC9PiO6ToOZCglrQJLlHfND6KY7KZCOuTYOOoQ6WEL8AhyNJgMF97syqwZDZD'
+const fb_version = 'v2.6'
 const start_time = new Date()
 
 /*
  * Post information route for a company post.
  */
 router.get('/post/:id', cache.route(), function (req, res, next) {
-  // start timer
   const post = req.params.id
-  let statistics = req.query.statistics ? req.query.statistics : 'id, type, message, created_time, likes'
-
+  let statistics = req.query.statistics ? req.query.statistics : 'id, type, message, created_time, likes.limit(0).summary(true), comments.limit(0).summary(true)'
+  statistics = preprocessPostQuery(statistics)
   fetch(`https://graph.facebook.com/${fb_version}/${post}/?fields=${statistics}&access_token=${access_token}`)
   .then(function (response) {
     // console.log(response)
@@ -30,7 +31,7 @@ router.get('/post/:id', cache.route(), function (req, res, next) {
         if (data.error) {
           res.json(failureResponseFormatter(req, 400, data.error.message))
         } else {
-          res.json(successResponseFormatter(req, response, data))
+          res.json(successResponseFormatter(req, response, formatPostInfo(data)))
         }
       })
     } else {
@@ -48,8 +49,6 @@ router.get('/post/:id', cache.route(), function (req, res, next) {
  * Facebook page information route for a company.
  */
 router.get('/:company', cache.route(), function (req, res, next) {
-     // start timer
-
   const statistics = req.query.statistics ? req.query.statistics : 'id, name, website, description, category, fan_count, posts'
 
   const start_date = moment(req.query.start_date)
@@ -124,7 +123,7 @@ const successResponseFormatter = (req, api_response, api_data) => {
   }
 }
 
-failureResponseFormatter = (req, response_code, status_text) => {
+const failureResponseFormatter = (req, response_code, status_text) => {
   const metadata = metaDataGen(req, response_code, status_text)
   return {
     data: [],
@@ -144,6 +143,41 @@ const metaDataGen = (req, response_code, status_text, api_data = null) => {
     status: response_code,
     status_text: status_text
   }
+}
+
+/*
+ * Converts user's paramaters into Facebook's equivalent for post query.
+ */
+const preprocessPostQuery = (params) => {
+  params = params.replace(/\blike_count\b/, 'likes.limit(0).summary(true)')
+  params = params.replace(/\bcomment_count\b/, 'comments.limit(0).summary(true)')
+  return params
+}
+
+/*
+ * Returns JSON containing only required info about the post.
+ * Used to tidy the output returned by the Facebook Graph API.
+ */
+const formatPostInfo = (post) => {
+  var ret = {}
+  var params = {
+    id: 'id',
+    type: 'type',
+    message: 'message',
+    created_time: 'created_time',
+    like_count: 'likes.summary.total_count',
+    comment_count: 'comments.summary.total_count'
+  }
+  for (p in params) {
+    if (p === 'like_count') {
+      if (post.likes !== undefined) ret[p] = post.likes.summary.total_count
+    } else if (p === 'comment_count') {
+      if (post.comments !== undefined) ret[p] = post.comments.summary.total_count
+    } else {
+      if (post[params[p]] !== undefined) ret[p] = post[params[p]]
+    }
+  }
+  return ret
 }
 
 const db = new sqlite3.Database('allcompanies.db', (err) => {
