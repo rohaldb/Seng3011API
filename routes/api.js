d1d194d9c3ca79d5fa3f264f4b0455dbb88be1a8
Adam Goldberg <gold.adamb@gmail.com>
2018-03-29 18:12:10 +1100

    uploaded database logic

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 var fetch = require('isomorphic-fetch')
 var express = require('express')
 var moment = require('moment')
+var sqlite3 = require('sqlite3').verbose()
 var router = express.Router()
 
-const access_token = 'EAACEdEose0cBACp14l1b0G1RCQR7uW4ypBcQrGxZA6gRbfE5iAZCavZAHdaqzxpuT8ABmfwseol68ZAZAI1XCCvPdKBIT293dHvzHuSYHnTN85l1tgg6dvgwKA9Cj9U1Xb8uUTyqolNm218KksPlQarfoGprcurgpdi7t3GFfeqiJR4FXYTUy1LXy8ZBur2iwZD'
+const access_token = 'EAACEdEose0cBACl0eSqZAwcUGJip7A3QCYIW63BooW3T6gCv8PCCsKxVOasp1MFB93Hyde4YC2eaL1SEplhUy1MYAerzNDGYSJ8C5yP5esEEpeZAMcK2ovT2hQD4IMZAWZAisBZCVZCqZBJAZB2rNudp0CpXGkMeFBgwT5nfdSel7YDAxiesKPAmBdEmcVO78vEZD'
 const fb_version = 'v2.6'
 
 /*
@@ -17,10 +18,10 @@ router.get('/post/:id', function (req, res, next) {
 
   if (!post) {
     // check for missing id parameter
-    res.json({status: 400, message: 'Missing parameter: `id`'})
+    res.json({status: 400, status_text: 'Missing parameter: `id`'})
   } else if (!statistics) {
     // check for missing stats parameter
-    res.json({status: 400, message: 'Missing parameter: `statistics`'})
+    res.json({status: 400, status_text: 'Missing parameter: `statistics`'})
   } else {
     fetch(`https://graph.facebook.com/${fb_version}/${post}/?fields=${statistics}&access_token=${access_token}`)
     .then(function (response) {
@@ -28,7 +29,7 @@ router.get('/post/:id', function (req, res, next) {
       if (response.ok) {
         response.json().then(data => {
           if (data.error) {
-            res.json({status: 400, message: data.error.message})
+            res.json({status: 400, status_text: data.error.message})
           } else {
             res.json(responseFormatter(req, response, start_time, data))
           }
@@ -36,7 +37,7 @@ router.get('/post/:id', function (req, res, next) {
       } else {
         response.json().then(data => {
           if (data.error) {
-            res.json({status: 400, message: data.error.message})
+            res.json({status: 400, status_text: data.error.message})
           }
         })
       }
@@ -48,57 +49,68 @@ router.get('/post/:id', function (req, res, next) {
 /*
  * Facebook page information route for a company.
  */
-router.get('/:company', function (req, res, next) {
-  // start timer
-  const start_time = new Date()
+ router.get('/:company', function (req, res, next) {
+     // start timer
+     const start_time = new Date()
+     let company = req.params.company
+     const statistics = req.query.statistics
+     const start_date = moment(req.query.start_date)
+     const end_date = moment(req.query.end_date)
 
-  // console.log(req.query)
+     db.each(`SELECT *, COUNT(1) > 0
+         FROM ListedCompanies
+         WHERE Code = '${company}' LIMIT 1`, (err, row) => {
+             if (err) {
+                 console.error(err.message);
+             }
+                // console.log(row.Code + "\t" + row.Company + "\t" + row.Industry);
+             if (row.Code != null) {
+                company = row.Company
+                // console.log(company)
+             }
+             if (!start_date.isValid() || !end_date.isValid()) {
+                 // check for valid date parameters
+                 res.json({status: 400, status_text: 'Invalid date parameters'})
+             } else if (!statistics) {
+                 // check for missing stats parameter
+                 res.json({status: 400, status_text: 'Missing parameter: `statistics`'})
+             } else {
+                 fetch(graphAPIString(company, start_date, end_date, statistics))
+                 .then(response => {
+                     if (response.ok) {
+                         response.json().then(data => {
+                             res.json(responseFormatter(req, response, start_time, data))
+                         })
+                     } else {
+                         return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
+                     }
+                 }).then(response => {
+                     if (response) {
+                         response.json().then(data => {
+                             if (data.data[0]) {
+                                 const companyId = data.data[0].id
+                                 return fetch(graphAPIString(companyId, start_date, end_date, statistics))
+                             } else {
+                                 res.json(responseFormatter(req, response))
+                             }
+                         }).then(response => {
+                             if (response) {
+                                 response.json().then(data => {
+                                     if (data.error) {
+                                         res.json({status: 400, status_text: data.error.message})
+                                     } else {
+                                         res.json(responseFormatter(req, response, start_time, data))
+                                     }
+                                 })
+                             }
+                         })
+                     }
+                 }).catch(error => console.error(error))
+             }
 
-  const company = req.params.company
-  const statistics = req.query.statistics
-  const start_date = moment(req.query.start_date)
-  const end_date = moment(req.query.end_date)
+         })
 
-  if (!start_date.isValid() || !end_date.isValid()) {
-    // check for valid date parameters
-    res.json({status: 400, message: 'Invalid date parameters'})
-  } else if (!statistics) {
-    // check for missing stats parameter
-    res.json({status: 400, message: 'Missing parameter: `statistics`'})
-  } else {
-    fetch(graphAPIString(company, start_date, end_date, statistics))
-    .then(response => {
-      if (response.ok) {
-        response.json().then(data => {
-          res.json(responseFormatter(req, response, start_time, data))
-        })
-      } else {
-        return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
-      }
-    }).then(response => {
-      if (response) {
-        response.json().then(data => {
-          if (data.data[0]) {
-            const companyId = data.data[0].id
-            return fetch(graphAPIString(companyId, start_date, end_date, statistics))
-          } else {
-            res.json(responseFormatter(req, response))
-          }
-        }).then(response => {
-          if (response) {
-            response.json().then(data => {
-              if (data.error) {
-                res.json({status: 400, message: data.error.message})
-              } else {
-                res.json(responseFormatter(req, response, start_time, data))
-              }
-            })
-          }
-        })
-      }
-    }).catch(error => console.error(error))
-  }
-})
+     })
 
 /*
  * Returns URL to pass to Facebook Graph API for company info.
@@ -114,7 +126,7 @@ const responseFormatter = (req, api_response, start_time, api_data) => {
   const end_time = new Date()
   return {
     data: api_data,
-    metadata: {
+    // metadata: {
       dev_team: 'Team Unassigned',
       version: '1.0.0',
       start_time,
@@ -123,8 +135,17 @@ const responseFormatter = (req, api_response, start_time, api_data) => {
       params: req.query,
       status: api_response.status,
       status_text: api_response.statusText
-    }
+    // }
   }
 }
+
+let db = new sqlite3.Database('allcompanies.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+
+
+});
+
 
 module.exports = router
