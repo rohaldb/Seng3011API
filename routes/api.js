25a68000b0d31e45bc4ac3cbeca83bff3b5492bf
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-03-30 13:15:31 +1100

    refactor backend, tidy posts array for company endpoint, correctly deal with nested queries, make queries canonical

--- routes/api.js
+++ routes/api.js
@@ -12,17 +12,19 @@ var cache = require('express-redis-cache')({
   client: require('redis').createClient(REDIS_URL)
 });
 
-const access_token = 'EAACEdEose0cBAOoIZA8W8oIWm4FwXKZAjwKyh4gyMlsf7Ra6Y1VkymhHLEMGY5ebZCc7QnwhZAJVu9ilyzIJtxdOoy4s9L970b6bOgONrPP4BYllFGetj5onz5lIkpq7xqvu0IjnZB9CSJPKnC2Xhi3Yy0EtbPLz8fK37D8IDBfxjFQ1pPziXqebfXDkWbhl2KICuiBSP2wZDZD'
+const access_token = 'EAACEdEose0cBAIUrypWQoloLuTPsXyJIimViaT0dL5RIbAgux2AupGO0LDsj5IJaCKPHIhJ7j5RYlA1M4QQVw7f15FSVZBZCA1ktRYQ9MGwVQGE7jKg5PTNndALqSFM8UkVHuOgRD4nbQZCI8wRmjN4tJqPNmzPsnSb2YR9EZAvAHu7hf3VTZAXEzoSU4bGsiLId4XQHxCQZDZD'
 const fb_version = 'v2.6'
 const start_time = new Date()
+const defaultPostParams = 'id, type, message, created_time, likes.limit(0).summary(true), comments.limit(0).summary(true)'
+const defaultCompanyParams = `id, name, website, description, category, fan_count, posts \{${defaultPostParams}\}`
 
 /*
  * Post information route for a company post.
  */
 router.get('/post/:id', cache.route(), function (req, res, next) {
   const post = req.params.id
-  let statistics = req.query.statistics ? req.query.statistics : 'id, type, message, created_time, likes.limit(0).summary(true), comments.limit(0).summary(true)'
-  statistics = preprocessPostQuery(statistics)
+  let statistics = req.query.statistics || defaultPostParams
+  statistics = preprocessQuery(statistics)
   fetch(`https://graph.facebook.com/${fb_version}/${post}/?fields=${statistics}&access_token=${access_token}`)
   .then(function (response) {
     // console.log(response)
@@ -49,14 +51,15 @@ router.get('/post/:id', cache.route(), function (req, res, next) {
  * Facebook page information route for a company.
  */
 router.get('/:company', cache.route(), function (req, res, next) {
-  const statistics = req.query.statistics ? req.query.statistics : 'id, name, website, description, category, fan_count, posts'
+  let statistics = req.query.statistics || defaultCompanyParams
+  statistics = preprocessQuery(statistics)
 
   const start_date = moment(req.query.start_date)
   const end_date = moment(req.query.end_date)
 
   db.each(`SELECT *, COUNT(1) > 0
-         FROM ListedCompanies
-         WHERE Code = '${req.params.company}' LIMIT 1`, (err, row) => {
+  FROM ListedCompanies
+  WHERE Code = '${req.params.company}' LIMIT 1`, (err, row) => {
     if (err) {
       console.error(err.message)
     }
@@ -68,36 +71,36 @@ router.get('/:company', cache.route(), function (req, res, next) {
       res.json(failureResponseFormatter(req, 400, 'Invalid date parameters'))
     } else {
       fetch(graphAPIString(company, start_date, end_date, statistics))
-       .then(response => {
-         if (response.ok) {
-           response.json().then(data => {
-             res.json(successResponseFormatter(req, response, data))
-           })
-         } else {
-           return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
-         }
-       }).then(response => {
-         if (response) {
-           response.json().then(data => {
-             if (data.data[0]) {
-               const companyId = data.data[0].id
-               return fetch(graphAPIString(companyId, start_date, end_date, statistics))
-             } else {
-               res.json(successResponseFormatter(req, response))
-             }
-           }).then(response => {
-             if (response) {
-               response.json().then(data => {
-                 if (data.error) {
-                   res.json(failureResponseFormatter(req, 400, data.error.message))
-                 } else {
-                   res.json(successResponseFormatter(req, response, data))
-                 }
-               })
-             }
-           })
-         }
-       }).catch(error => console.error(error))
+      .then(response => {
+        if (response.ok) {
+          response.json().then(data => {
+            res.json(successResponseFormatter(req, response, formatPostInfo(data)))
+          })
+        } else {
+          return fetch(`https://graph.facebook.com/${fb_version}/search?q=${company}&type=page&fields=name,fan_count&access_token=${access_token}`)
+        }
+      }).then(response => {
+        if (response) {
+          response.json().then(data => {
+            if (data.data[0]) {
+              const companyId = data.data[0].id
+              return fetch(graphAPIString(companyId, start_date, end_date, statistics))
+            } else {
+              res.json(successResponseFormatter(req, response))
+            }
+          }).then(response => {
+            if (response) {
+              response.json().then(data => {
+                if (data.error) {
+                  res.json(failureResponseFormatter(req, 400, data.error.message))
+                } else {
+                  res.json(successResponseFormatter(req, response, formatPostInfo(data)))
+                }
+              })
+            }
+          })
+        }
+      }).catch(error => console.error(error))
     }
   })
 })
@@ -149,38 +152,32 @@ const metaDataGen = (req, response_code, status_text, api_data = null) => {
 }
 
 /*
- * Converts user's paramaters into Facebook's equivalent for post query.
+ * Converts user's paramaters for likes and comments into Facebook's equivalent.
  */
-const preprocessPostQuery = (params) => {
-  params = params.replace(/\blike_count\b/, 'likes.limit(0).summary(true)')
-  params = params.replace(/\bcomment_count\b/, 'comments.limit(0).summary(true)')
+const preprocessQuery = (params) => {
+  params = params.replace(/\blikes\b/, 'likes.limit(0).summary(true)')
+  params = params.replace(/\bcomments\b/, 'comments.limit(0).summary(true)')
   return params
 }
 
 /*
- * Returns JSON containing only required info about the post.
+ * Reformats JSON result for post information.
  * Used to tidy the output returned by the Facebook Graph API.
  */
-const formatPostInfo = (post) => {
-  var ret = {}
-  var params = {
-    id: 'id',
-    type: 'type',
-    message: 'message',
-    created_time: 'created_time',
-    like_count: 'likes.summary.total_count',
-    comment_count: 'comments.summary.total_count'
+const formatPostInfo = (data) => {
+  //tidy likes and comments for a single post
+  if (data.likes !== undefined) data.likes = data.likes.summary.total_count
+  if (data.comments !== undefined) data.comments = data.comments.summary.total_count
+
+  //recursively tidy posts array
+  if (data.posts === undefined) return data
+  var newPosts = []
+  for (post of data.posts.data) {
+    newPosts.push(formatPostInfo(post))
   }
-  for (p in params) {
-    if (p === 'like_count') {
-      if (post.likes !== undefined) ret[p] = post.likes.summary.total_count
-    } else if (p === 'comment_count') {
-      if (post.comments !== undefined) ret[p] = post.comments.summary.total_count
-    } else {
-      if (post[params[p]] !== undefined) ret[p] = post[params[p]]
-    }
-  }
-  return ret
+
+  data.posts = newPosts
+  return data
 }
 
 /*
