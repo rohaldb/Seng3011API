ea80763a82d0bcad2b98d6f59eb0fea1731f53c2
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-04-18 15:20:35 +1000

    update routing to support versioning

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
 var api = require('./routes/api')
+var api1 = require('./routes/api1')
+var api2 = require('./routes/api2')
 
 var app = express()
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/api', api)
+app.use('/api/v1', api1)
+app.use('/api/v2', api2)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  /* res.render('index', { title: 'Express' }); */
+  res.redirect('https://seng4-a904d.firebaseapp.com/');
 });
 
 module.exports = router;
