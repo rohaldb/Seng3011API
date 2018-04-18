103652b07af462cbe4aba212522f93834140aa09
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-04-18 16:04:06 +1000

    fix some bugs, validate params correctly

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   /* res.render('index', { title: 'Express' }); */
+  /* redirect to team page for now */
   res.redirect('https://seng4-a904d.firebaseapp.com/');
 });
 
