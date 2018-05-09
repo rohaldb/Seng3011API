b07818d3104aeb332bf91fbc3f43cc71238110c2
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-05-09 18:48:34 +1000

    specify google chrome location for gen_token since non-standard in buildpack

--- gen_token.py
+++ gen_token.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.options import Options
 #navigate to fb login page
 options = Options()
 options.add_argument('--headless')
+options.binary_location = os.environ('GOOGLE_CHROME_BIN')
 driver = webdriver.Chrome('bin/chromedriver', options=options)
 driver.get('https://www.facebook.com/')
 
