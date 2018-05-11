8e0a0f60a5a22ebc8c15e880e3810661f1486b14
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-05-11 11:17:35 +1000

    regen api token on 1st request after idle & fix start_time global variable bug

--- routes/api4.js
+++ routes/api4.js
@@ -5,15 +5,17 @@ var sqlite3 = require('sqlite3').verbose()
 var router = express.Router()
 var cron = require('node-cron')
 var fs = require('fs')
+var util = require('util')
 
 /*
  * Use Heroku REDIS_URL or default to local redis server.
+ * The start_time and end_time will reflect the original request if cached.
  */
 var REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 var cache = require('express-redis-cache')({
   client: require('redis').createClient(REDIS_URL),
     expire: {
-      200: 300, /* expire successful requests 5 minutes */
+      200: 300, /* expire successful requests every 5 minutes */
       400: 1, /* expire these asap as a workaround to redis mapping 400 -> 200 if cached */
       xxx: 20 /* expire anything else every 20 seconds */
     }
@@ -21,7 +23,6 @@ var cache = require('express-redis-cache')({
 
 var access_token = ''
 const fb_version = 'v2.6'
-const start_time = new Date()
 const defaultPostParams = 'id, type, message, created_time, likes, comments'
 const defaultCompanyParams = `id, name, website, description, category, fan_count, posts \{${defaultPostParams}\}`
 
@@ -29,12 +30,14 @@ const defaultCompanyParams = `id, name, website, description, category, fan_coun
  * Report error if unspecified endpoint.
  */
 router.get('/', cache.route(), function (req, res, next) {
+  const start_time = new Date()
   res.status(400)
-  res.json(failureResponseFormatter(req, 400, 'No endpoint specified'))
+  res.json(failureResponseFormatter(start_time, req, 400, 'No endpoint specified'))
 })
 router.get('/api', cache.route(), function (req, res, next) {
+  const start_time = new Date()
   res.status(400)
-  res.json(failureResponseFormatter(req, 400, 'No endpoint specified'))
+  res.json(failureResponseFormatter(start_time, req, 400, 'No endpoint specified'))
 })
 
 /*
@@ -44,59 +47,83 @@ router.get('/api', cache.route(), function (req, res, next) {
  * of memory, so 5 minutes is a reasonable balance.
  */
 cron.schedule('*/5 * * * *', function() {
+  genToken(function(err) {
+    console.log(err)
+  })
+})
+
+/*
+ * Update the Facebook API token in .token.
+ */
+const genToken = (callback) => {
   const { spawn } = require('child_process')
   const prog = spawn('python3', ['./gen_token.py'])
   prog.stderr.on('data', function(data) {
     console.log('error running gen_token: ' + data)
+    callback('error')
   })
   prog.stdout.on('data', function(data) {
     fs.writeFile('.token', data.toString(), function(err) {
       if (err) return console.log('failed to update api token: ' + err)
       console.log('updated api token: ' + data.toString())
+      callback('success')
     })
   })
-})
+}
 
 /*
  * If hidden workaround is specified, use the token in the .token file.
  * Otherwise, ensure a token is provided and if so, use that instead.
  */
 const getAccessToken = (req, res, callback) => {
-  if (req.query.workaround) {
-    fs.readFile('.token', 'utf8', function (err, data) {
-      if (err) return console.log(err)
-      callback(data.toString().replace(/\n$/, ''))
-    })
-  } else {
-    if (!req.query.access_token || req.query.access_token.length < 10) {
-      /* ensure an access token is provided */
-      callback('error')
+  var fn = function(err) {
+    if (req.query.workaround) {
+      fs.readFile('.token', 'utf8', function (err, data) {
+        if (err) return console.log(err)
+        callback(data.toString().replace(/\n$/, ''))
+      })
     } else {
-      callback(req.query.access_token)
+      if (!req.query.access_token || req.query.access_token.length < 10) {
+        /* ensure an access token is provided */
+        callback('error')
+      } else {
+        callback(req.query.access_token)
+      }
     }
   }
+  fs.stat('.token', function(err, stats) {
+    /* if server was in idle, .token may be expired, so regenerate first */
+    var mtime = new Date(util.inspect(stats.mtime)).getTime() / 1000
+    var now = new Date() / 1000
+    if (now - mtime > 800) {
+      genToken(function(err) {
+        fn(err)
+      })
+    } else {
+      fn('success')
+    }
+  })
 }
 
 /*
  * Post information route for a company post.
  */
 router.get('/post/:id', cache.route(), function (req, res, next) {
+  const start_time = new Date()
   const post = req.params.id
   let statistics = req.query.statistics || defaultPostParams
   getAccessToken(req, res, function(token) {
     if (token === 'error') {
       res.status(400)
-      res.json(failureResponseFormatter(req, 400, 'Missing parameter `access_token`'))
+      res.json(failureResponseFormatter(start_time, req, 400, 'Missing parameter `access_token`'))
     } else {
       access_token = token
       console.log(access_token)
     }
-  })
-  setTimeout(function() {
     if (validPostStats(statistics) !== '') {
       /* check for valid stats parameter */
       res.status(400)
-      res.json(failureResponseFormatter(req, 400, validPostStats(statistics)))
+      res.json(failureResponseFormatter(start_time, req, 400, validPostStats(statistics)))
     }
     statistics = preprocessQuery(statistics)
 
@@ -106,16 +133,16 @@ router.get('/post/:id', cache.route(), function (req, res, next) {
         response.json().then(data => {
           if (data.error) {
             res.status(400)
-            res.json(failureResponseFormatter(req, 400, data.error.message))
+            res.json(failureResponseFormatter(start_time, req, 400, data.error.message))
           } else {
-            res.json(successResponseFormatter(req, response, formatPostInfo(data)))
+            res.json(successResponseFormatter(start_time, req, response, formatPostInfo(data)))
           }
         })
       } else {
         response.json().then(data => {
           if (data.error) {
             res.status(400)
-            res.json(failureResponseFormatter(req, 400, data.error.message))
+            res.json(failureResponseFormatter(start_time, req, 400, data.error.message))
           }
         })
       }
@@ -128,21 +155,20 @@ router.get('/post/:id', cache.route(), function (req, res, next) {
  * Facebook page information route for a company.
  */
 router.get('/:company', cache.route(), function (req, res, next) {
+  const start_time = new Date()
   let statistics = req.query.statistics || defaultCompanyParams
   getAccessToken(req, res, function(token) {
     if (token === 'error') {
       res.status(400)
-      res.json(failureResponseFormatter(req, 400, 'Missing parameter `access_token`'))
+      res.json(failureResponseFormatter(start_time, req, 400, 'Missing parameter `access_token`'))
     } else {
       access_token = token
       console.log(access_token)
     }
-  })
-  setTimeout(function() {
     if (validCompanyStats(statistics) != '') {
       /* check for valid stats parameter */
       res.status(400)
-      res.json(failureResponseFormatter(req, 400, validCompanyStats(statistics)))
+      res.json(failureResponseFormatter(start_time, req, 400, validCompanyStats(statistics)))
     }
     statistics = preprocessQuery(statistics)
 
@@ -165,11 +191,11 @@ router.get('/:company', cache.route(), function (req, res, next) {
       if (!start_date.isValid() || !end_date.isValid()) {
         /* check for valid date parameters */
         res.status(400)
-        res.json(failureResponseFormatter(req, 400, 'Invalid date parameters'))
+        res.json(failureResponseFormatter(start_time, req, 400, 'Invalid date parameters'))
       } else if (has_start && has_end && !end_date.isAfter(start_date)) {
         /* check that start_date <= end_date */
         res.status(400)
-        res.json(failureResponseFormatter(req, 400, 'start_date must precede end_date'))
+        res.json(failureResponseFormatter(start_time, req, 400, 'start_date must precede end_date'))
       } else {
         /* otherwise, proceed with query */
         console.log(companyAPIString(company, statistics))
@@ -179,7 +205,7 @@ router.get('/:company', cache.route(), function (req, res, next) {
             response.json().then(data => {
               if (!statistics.match(/posts/)) {
                 /* format data immediately if posts not requested */
-                res.json(successResponseFormatter(req, response, formatPostInfo(data)))
+                res.json(successResponseFormatter(start_time, req, response, formatPostInfo(data)))
               } else {
                 console.log(postAPIString(company, start_date, end_date, has_start && has_end, statistics.replace(/.*posts\ *\{|}.*/g, '')))
                 fetch(postAPIString(company, start_date, end_date, has_start && has_end, statistics.replace(/.*posts\ *\{|}.*/g, '')))
@@ -187,26 +213,26 @@ router.get('/:company', cache.route(), function (req, res, next) {
                   if (response.ok) {
                     response.json().then(posts => {
                       if (posts) data['posts'] = posts /* combine posts with company data */
-                      res.json(successResponseFormatter(req, response, formatPostInfo(data)))
+                      res.json(successResponseFormatter(start_time, req, response, formatPostInfo(data)))
                     }).catch(error => console.error(error))
                   } else {
                     /* some unknown error occured - this is very unlikely to occur */
                     res.status(400)
-                    res.json(failureResponseFormatter(req, 400, 'An unknown error occured'))
+                    res.json(failureResponseFormatter(start_time, req, 400, 'An unknown error occured'))
                   }
                 })
               }
             }).catch(error => console.error(error))
           } else {
             response.json().then(data => {
-              if (data && data.error && data.error.message && data.error.message.match(/access token|temporarily disabled/)) {
+              if (data && data.error && data.error.message && data.error.message.match(/access token|temporarily disabled/i)) {
                 /* inform users of invalid or expired access token */
                 res.status(400)
-                res.json(failureResponseFormatter(req, 400, 'Access token invalid or expired'))
+                res.json(failureResponseFormatter(start_time, req, 400, 'Access token invalid or expired'))
               } else {
                 /* somewhat informative error */
                 res.status(400)
-                res.json(failureResponseFormatter(req, 400, `Unknown company \`${req.params.company}\``))
+                res.json(failureResponseFormatter(start_time, req, 400, `Unknown company \`${req.params.company}\``))
               }
             }).catch(error => console.error(error))
           }
@@ -237,8 +263,8 @@ const postAPIString = (company, start_date, end_date, pass_date, statistics) => 
 /*
  * Returns response JSON containing result data and metadata.
  */
-const successResponseFormatter = (req, api_response, api_data) => {
-  const metadata = metaDataGen(req, api_response.status, api_response.statusText, api_data)
+const successResponseFormatter = (start_time, req, api_response, api_data) => {
+  const metadata = metaDataGen(start_time, req, api_response.status, api_response.statusText, api_data)
   return {
     data: api_data,
     ...metadata
@@ -248,8 +274,8 @@ const successResponseFormatter = (req, api_response, api_data) => {
 /*
  * Returns error response JSON containing metadata and empty data.
  */
-const failureResponseFormatter = (req, response_code, status_text) => {
-  const metadata = metaDataGen(req, response_code, status_text)
+const failureResponseFormatter = (start_time, req, response_code, status_text) => {
+  const metadata = metaDataGen(start_time, req, response_code, status_text)
   return {
     data: [],
     ...metadata
@@ -259,7 +285,7 @@ const failureResponseFormatter = (req, response_code, status_text) => {
 /*
  * Returns metadata for the current API query.
  */
-const metaDataGen = (req, response_code, status_text, api_data = null) => {
+const metaDataGen = (start_time, req, response_code, status_text, api_data = null) => {
   const end_time = new Date()
   return {
     dev_team: 'Team Unassigned',
@@ -317,8 +343,7 @@ const validCompanyStats = (statistics) => {
     if (i > 0) {
       return 'Statistic `posts` specified more than once (not allowed since version 2)'
     }
-    if (!(s[0].match('^posts\ *\{[^,]+(,[^,]+)*\}$') && validPostStats(s[0]) === '')
-) {
+    if (!(s[0].match('^posts\ *\{[^,]+(,[^,]+)*\}$') && validPostStats(s[0]) === '')) {
       return `Unknown statistic: \`${s[0]}\``
     }
     statistics = statistics.replace(/posts\ *\{.+?\}/, '')
